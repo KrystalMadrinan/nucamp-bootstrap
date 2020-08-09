35abea9ff209acc6b34c7a0af0a646a8590b44fc
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2020-08-09 14:56:43 -0700

    Bootstrap and jQuery Part 1

--- app.js
+++ app.js
@@ -5,3 +5,16 @@
 // $(function () {
 //     $('[data-toggle="tooltip"]').tooltip()
 // });
+
+
+// CAROUSEL CONTROLS
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselPause").click(function(){
+        $(".carousel").carousel("pause");
+    });
+    $("#carouselPlay").click(function(){
+        $(".carousel").carousel("cycle");
+    });
+});
