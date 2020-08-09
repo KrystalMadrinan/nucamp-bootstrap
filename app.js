956e0bc82d5c4d55a6ebc482e0baca5abab7e6bc
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2020-08-09 15:02:45 -0700

    Bootstrap and jQuery Part 2

--- app.js
+++ app.js
@@ -9,12 +9,17 @@
 
 // CAROUSEL CONTROLS
 
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselPause").click(function(){
-        $(".carousel").carousel("pause");
-    });
-    $("#carouselPlay").click(function(){
-        $(".carousel").carousel("cycle");
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
     });
 });
